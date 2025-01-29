d368b13f4ae04f921d1fa2bbe41c8ba7916238b4
Rain_zzx <1843750745@qq.com>
2025-01-29 20:40:53 +0800

    mouse_key event

---
+++ 7-mouse_key/widget.cpp
@@ -0,0 +1,69 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+
+    status = new QLabel(this);
+    mousePos = new QLabel(this);
+    mouseEvent = new QLabel(this);
+    keyEvent = new QLabel(this);
+    status->setText(u8"鼠标当前位置：");
+    status->setFixedWidth(200);
+
+    mousePos->setText("");
+    mousePos->setFixedWidth(200);
+
+    QStatusBar * statusBar = new QStatusBar(this);
+    statusBar->addPermanentWidget(status);
+    statusBar->addPermanentWidget(mousePos);
+    statusBar->addPermanentWidget(mouseEvent);
+    statusBar->addPermanentWidget(keyEvent);
+
+    this->setMouseTracking(true);
+
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *e)
+{
+    mousePos->setText("("+QString::number(e->x())+","+QString::number(e->y())+")");
+}
+
+void Widget::mousePressEvent(QMouseEvent *e)
+{
+    if(e->button()==Qt::LeftButton)
+    {
+        mouseEvent->setText(u8"用户按下左键");
+    }
+    else if(e->button() == Qt::RightButton)
+    {
+
+        mouseEvent->setText(u8"用户按下右键");
+    }
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *e)
+{
+    mouseEvent->setText(u8"用户松开按键");
+}
+
+void Widget::keyPressEvent(QKeyEvent *e)
+{
+    if(e->key() == Qt::Key_Left)
+    {
+        keyEvent->setText(u8"按下左键");
+    }
+    else if(e->key() == Qt::Key_K)
+    {
+        keyEvent->setText(u8"按下K键");
+    }
+}
+
---
+++ 7-mouse_key/widget.h
@@ -0,0 +1,36 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QStatusBar>
+#include <QMouseEvent>
+#include <QKeyEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    void mouseMoveEvent(QMouseEvent*e);
+    void mousePressEvent(QMouseEvent*e);
+    void mouseReleaseEvent(QMouseEvent*e);
+
+    void keyPressEvent(QKeyEvent*e);
+
+    QLabel* status ;
+    QLabel* mousePos;
+    QLabel* mouseEvent;
+    QLabel* keyEvent;
+
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
